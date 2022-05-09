b7d42c419dae764ea7e7afb07765e04ba924d3e1
Muftullo <mufasadullaev@gmail.com>
2022-05-09 14:06:37 +0500

    Solution Task1 added

---
+++ Task1/Task1/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 4;
+
+            for (; a < 10; a++)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
