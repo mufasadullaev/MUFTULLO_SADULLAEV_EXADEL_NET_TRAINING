b099785eb68639c7f8b70ebb17d6d15c624f0025
Muftullo <mufasadullaev@gmail.com>
2022-05-13 01:05:25 +0500

    Task2.2 first stage completed, strings from files can be read

--- Task2/Task2/Task2.2/Program.cs
+++ Task2/Task2/Task2.2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task2._2
 {
@@ -10,6 +11,13 @@ namespace Task2._2
     {
         static void Main(string[] args)
         {
+            string text1 = File.ReadAllText(@"C:\Users\hacke\MUFTULLO_SADULLAEV_EXADEL_NET_TRAINING\Task2\Task2\Task2.2\File1.txt");
+            Console.WriteLine(text1);
+            string text2 = File.ReadAllText(@"C:\Users\hacke\MUFTULLO_SADULLAEV_EXADEL_NET_TRAINING\Task2\Task2\Task2.2\File2.txt");
+            Console.WriteLine(text2);
+            string text3 = File.ReadAllText(@"C:\Users\hacke\MUFTULLO_SADULLAEV_EXADEL_NET_TRAINING\Task2\Task2\Task2.2\File3.txt");
+            Console.WriteLine(text3);
+            Console.ReadLine();
         }
     }
 }
