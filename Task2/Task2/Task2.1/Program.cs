2b6e0d46c5c1a190d748f4a14bbb599c759c4d16
Muftullo <mufasadullaev@gmail.com>
2022-05-12 18:57:45 +0500

    Projects added

---
+++ Task2/Task2/Task2.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 3;
+            while (a > 0)
+            {
+                for (int i = a - 1; i > 0; i--)
+                {
+                    a *=i;
+                }
+
+            }
+            
+        }
+    }
+}
