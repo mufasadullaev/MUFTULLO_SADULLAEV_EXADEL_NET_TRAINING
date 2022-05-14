68ce1bf1ee562ae2e201e54b6d0ff4180932c473
Muftullo <mufasadullaev@gmail.com>
2022-05-15 01:45:20 +0500

    Task2.2 is fully done. All comments fixed

--- Task2/Task2/Task2.2/Program.cs
+++ Task2/Task2/Task2.2/Program.cs
@@ -12,29 +12,24 @@ namespace Task2._2
     {
         static void Main(string[] args)
         {
-            DirectoryInfo d = new DirectoryInfo(@"C:\Users\hacke\MUFTULLO_SADULLAEV_EXADEL_NET_TRAINING\Task2\Task2\Task2.2");
-            string newFile = @"C:\Users\hacke\MUFTULLO_SADULLAEV_EXADEL_NET_TRAINING\Task2\Task2\Task2.2\NewFile.txt";
-            /*            string fileLoc = AppDomain.CurrentDomain.BaseDirectory;
-                        DirectoryInfo d = new DirectoryInfo(fileLoc);*/
-            
+            string fileLoc = $"{AppDomain.CurrentDomain.BaseDirectory}\\..\\..";
+            DirectoryInfo d = new DirectoryInfo($"{fileLoc}");
             FileInfo[] f = d.GetFiles("*.txt", SearchOption.TopDirectoryOnly);
-/*            string newFile = $"{fileLoc}\\NewFile.txt";*/
+            string newFile = $"{fileLoc}\\NewFile.txt";
             int fileNum = f.Length;
             List<string[]> result = new List<string[]>();
             for(int i = 0; i < fileNum; i++)
             {
-                /*string myText = File.ReadAllText($"{fileLoc}\\File{i}.txt");*/
-                string filepath = $"C:\\Users\\hacke\\MUFTULLO_SADULLAEV_EXADEL_NET_TRAINING\\Task2\\Task2\\Task2.2\\File{i}.txt";
-                string myText = File.ReadAllText($"{filepath}");
+                string myText = File.ReadAllText($"{fileLoc}\\File{i}.txt");
                 string myCleanTxt = Regex.Replace(myText, @"[^a-zA-Z\s]+", "");
                 string[] myResult = myCleanTxt.Split(' ');
+                string newReverse = "";
+                string[] endResult = { };
                 string uprLow = "";
                 Console.WriteLine($"\nInitial String: {myText}");
                 for (int x = myResult.Length - 1; x >= 0; x--)
                 {
-                    /*newReverse += myResult[x] + " ";*/
                     char[] myResultChar = myResult[x].ToCharArray();
-                    /*for (int y = myResultChar.Length - 1; x >= 0; x--)*/
                     int len = myResultChar.Length;
                     string upr = "";
                     string low = "";
@@ -59,10 +54,12 @@ namespace Task2._2
                     else
                     {
                         myResult[x] = uprLow;
+                        newReverse += myResult[x] + " ";
                     }
                 }
-                result.Add(myResult);
-                File.Delete($"{filepath}");
+                endResult = newReverse.Split(' ');
+                result.Add(endResult);
+                File.Delete($"{fileLoc}\\File{i}.txt");
             }
             result = result.OrderBy(i => i.Length).ToList();
             using (StreamWriter sw = File.CreateText(newFile))
