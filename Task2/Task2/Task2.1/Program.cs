71a7a1238be6e3789b5b129efff5d9f80eac1e73
Muftullo <mufasadullaev@gmail.com>
2022-05-15 02:15:45 +0500

    Task2.1 made the requested change on user inputs

--- Task2/Task2/Task2.1/Program.cs
+++ Task2/Task2/Task2.1/Program.cs
@@ -17,10 +17,19 @@ namespace Task2._1
             Console.Write("Calculate the factorial of a given number:\n");
             Console.Write("Input the number A: ");
             a = Convert.ToInt32(Console.ReadLine());
+            while (a < 0 || a > 5)
+            {
+                Console.Write("Input A must be from 0 to 5 inclusively!\nPlease, enter a valid number: ");
+                a = int.Parse(Console.ReadLine());
+            }
             Console.Write("Calculate the natural logarithm of a given number:\n");
-            Console.Write("Input the number B: ");
+            Console.Write("Input the number B: "); 
             b = Convert.ToInt32(Console.ReadLine());
-
+            while (b < 0 || b > 100)
+            {
+                Console.WriteLine("Input B must be from 0 to 100 inclusively!\nPlease, enter a valid number: ");
+                b = int.Parse(Console.ReadLine());
+            }
             if (a >= 0 && a <= 5 & b >= 0 && b <= 100)
             {
                 for (i = 1; i <= a; i++)
@@ -43,16 +52,6 @@ namespace Task2._1
                 {
                     Console.WriteLine($"Number 'a' = {aResult} and equal to number 'b' = {bResult}");
                 }
-            }
-            else if (a < 0 || a > 5)
-            {
-                Console.WriteLine("Input A must be from 0 to 5 inclusively!");
-
-            }
-
-            else 
-            {
-                Console.WriteLine("Input B must be from 0 to 100 inclusively!");
             }
             Console.ReadLine();
         }
