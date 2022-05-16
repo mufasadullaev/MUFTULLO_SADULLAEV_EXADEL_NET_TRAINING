15b1478b22dfbf47c14d0acdd73879fec7561960
Muftullo <mufasadullaev@gmail.com>
2022-05-17 01:30:54 +0500

    Task3.2 in process

--- Task3/Task3/Task3.2/Program.cs
+++ Task3/Task3/Task3.2/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Task3._2
 {
@@ -10,6 +12,17 @@ namespace Task3._2
     {
         static void Main(string[] args)
         {
+            using (StreamReader city = new StreamReader("../../city.json"))
+            using (StreamReader people = new StreamReader("../../inhabitant.json"))
+            {
+                string cityJson = city.ReadToEnd();
+                string peopleJson = people.ReadToEnd();
+                dynamic cityArray = JsonConvert.DeserializeObject(cityJson);
+                dynamic peopleArray = JsonConvert.DeserializeObject(peopleJson);
+
+            }
+            Console.ReadLine();
         }
+
     }
 }
