44a002f72e70649f476c804e15dbe11f5c43ac5c
Muftullo <mufasadullaev@gmail.com>
2022-05-16 17:03:13 +0500

    Task3 is done

---
+++ Task3/Task3/Task3.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+            var userInput = Console.ReadLine();
+            var cleanInput = Regex.Replace(userInput, @"[^(){}]", "");
+            int len = cleanInput.Length;
+            if ( len % 2 == 0)
+            {
+                Console.WriteLine($"Output: {cleanInput} - true");
+            }
+            else
+            {
+                Console.WriteLine($"Output: {cleanInput} - false");
+            }
+            Console.ReadLine();
+        }
+    }
+}
