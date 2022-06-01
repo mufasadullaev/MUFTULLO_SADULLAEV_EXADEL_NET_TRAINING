609abebfeba3233b5e80d502a6a06f51b9ff621f
Muftullo <mufasadullaev@gmail.com>
2022-06-01 22:03:57 +0500

    Task 6 Multi-thread method done

---
+++ Task6/Task6/Task6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int method;
+            Console.WriteLine(@"1. Asynchronous
+2. Multi-threaded");
+            Console.Write("Choose a method: ");
+            method = Convert.ToInt32(Console.ReadLine());
+            while (method != 1 && method != 2){
+                Console.WriteLine("Please, choose 1 or 2");
+                Console.WriteLine(@"1. Asynchronous
+2. Multi-threaded");
+                Console.Write("Choose a method: ");
+                method = int.Parse(Console.ReadLine());
+            }
+            if (method == 1)
+            {
+                Console.WriteLine("\nAsyncronion method");
+                Console.WriteLine();
+            }
+            else if (method == 2)
+            {
+                var watch = new System.Diagnostics.Stopwatch();
+                watch.Start();
+                Console.WriteLine("\nMulti-threaded method");
+                Console.WriteLine();
+                ThreadFunc threadFunc = new ThreadFunc();
+
+                Thread fileOneCaller = new Thread(new ThreadStart(threadFunc.FileOne));
+                fileOneCaller.Start();
+                fileOneCaller.Join();
+
+                Thread fileTwoCaller = new Thread(new ThreadStart(threadFunc.FileTwo));
+                fileTwoCaller.Start();
+                fileTwoCaller.Join();
+                watch.Stop();
+                Console.WriteLine($"\nExecution Time: {watch.ElapsedMilliseconds} ms");
+            }
+        }
+    }
+}
---
+++ Task6/Task6/Task6/ThreadFunc.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Task6
+{
+    public class ThreadFunc
+    {
+        public void FileOne()
+        {
+            string filePath = $"{AppDomain.CurrentDomain.BaseDirectory}\\..\\..\\..\\FileOne.txt";
+            string fileContent = File.ReadLines(filePath).First();
+            string[] fileNumbers = fileContent.Split(',');
+            int[] newArray = Array.ConvertAll(fileNumbers, int.Parse);
+            string mathOperator = File.ReadLines(filePath).Last();
+            Console.Write("Data from file 1: ");
+            switch (mathOperator)
+            {
+                case "+":
+                    for (int i = 0; i < newArray.Length; i++)
+                    {
+                        if (newArray[i] < 0)
+                        {
+                            Console.Write($"({newArray[i]})");
+                        }
+                        else
+                        {
+                            Console.Write(newArray[i]);
+                        }
+                        while (i != newArray.Length - 1)
+                        {
+                            Console.Write("+");
+                            break;
+                        }
+                    }
+                    Console.Write("= " + newArray.Aggregate((x, y) => x + y));
+                    break;
+                case "-":
+                    for (int i = 0; i < newArray.Length; i++)
+                    {
+                        if (newArray[i] < 0)
+                        {
+                            Console.Write($"({newArray[i]})");
+                        }
+                        else
+                        {
+                            Console.Write(newArray[i]);
+                        }
+                        while (i != newArray.Length - 1)
+                        {
+                            Console.Write("-");
+                            break;
+                        }
+                    }
+                    Console.Write("= " + newArray.Aggregate((x, y) => x - y));
+                    break;
+                case "*":
+                    for (int i = 0; i < newArray.Length; i++)
+                    {
+                        if (newArray[i] < 0)
+                        {
+                            Console.Write($"({newArray[i]})");
+                        }
+                        else
+                        {
+                            Console.Write(newArray[i]);
+                        }
+                        while (i != newArray.Length - 1)
+                        {
+                            Console.Write("*");
+                            break;
+                        }
+                    }
+                    Console.Write("= " + newArray.Aggregate((x, y) => x * y));
+                    break;
+                case "/":
+                    for (int i = 0; i < newArray.Length; i++)
+                    {
+                        if (newArray[i] < 0)
+                        {
+                            Console.Write($"({newArray[i]})");
+                        }
+                        else
+                        {
+                            Console.Write(newArray[i]);
+                        }
+                        while (i != newArray.Length - 1)
+                        {
+                            Console.Write("/");
+                            break;
+                        }
+                    }
+                    Console.Write("= " + newArray.Aggregate((x, y) => x / y));
+                    break;
+            }
+        }
+        public void FileTwo()
+        {
+            string filePath = $"{AppDomain.CurrentDomain.BaseDirectory}\\..\\..\\..\\FileTwo.txt";
+            string fileContent = File.ReadLines(filePath).First();
+            string[] fileNumbers = fileContent.Split(',');
+            int[] newArray = Array.ConvertAll(fileNumbers, int.Parse);
+            string mathOperator = File.ReadLines(filePath).Last();
+            Console.Write("\nData from file 2: ");
+            switch (mathOperator)
+            {
+                case "+":
+                    for (int i = 0; i < newArray.Length; i++)
+                    {
+                        if (newArray[i] < 0)
+                        {
+                            Console.Write($"({newArray[i]})");
+                        }
+                        else
+                        {
+                            Console.Write(newArray[i]);
+                        }
+                        while (i != newArray.Length - 1)
+                        {
+                            Console.Write("+");
+                            break;
+                        }
+                    }
+                    Console.Write("= " + newArray.Aggregate((x, y) => x + y));
+                    break;
+                case "-":
+                    for (int i = 0; i < newArray.Length; i++)
+                    {
+                        if (newArray[i] < 0)
+                        {
+                            Console.Write($"({newArray[i]})");
+                        }
+                        else
+                        {
+                            Console.Write(newArray[i]);
+                        }
+                        while (i != newArray.Length - 1)
+                        {
+                            Console.Write("-");
+                            break;
+                        }
+                    }
+                    Console.Write("= " + newArray.Aggregate((x, y) => x - y));
+                    break;
+                case "*":
+                    for (int i = 0; i < newArray.Length; i++)
+                    {
+                        if (newArray[i] < 0)
+                        {
+                            Console.Write($"({newArray[i]})");
+                        }
+                        else
+                        {
+                            Console.Write(newArray[i]);
+                        }
+                        while (i != newArray.Length - 1)
+                        {
+                            Console.Write("*");
+                            break;
+                        }
+                    }
+                    Console.Write("= " + newArray.Aggregate((x, y) => x * y));
+                    break;
+                case "/":
+                    for (int i = 0; i < newArray.Length; i++)
+                    {
+                        if (newArray[i] < 0)
+                        {
+                            Console.Write($"({newArray[i]})");
+                        }
+                        else
+                        {
+                            Console.Write(newArray[i]);
+                        }
+                        while (i != newArray.Length - 1)
+                        {
+                            Console.Write("/");
+                            break;
+                        }
+                    }
+                    Console.Write("= " + newArray.Aggregate((x, y) => x / y));
+                    break;
+            }
+        }
+    }
+}
